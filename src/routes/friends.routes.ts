4161e1b2a595affc843dd6a3577c6580971beea8
Kevin Alejandro Manzano Torres <manzanokevin5@gmail.com>
2026-01-24 11:32:05 -0500

    add fixes

--- src/routes/friends.routes.ts
+++ src/routes/friends.routes.ts
@@ -8,19 +8,24 @@ import {
   sendFriendRequest,
 } from '../controllers/friends.controller.ts';
 import { authMiddleware } from '../middlewares/auth.ts';
-import { acceptFriendRequestParamsDTO } from '../dtos/friend-request-params.ts';
+import { acceptRejectFriendRequestParamsDTO } from '../dtos/friend-request-params.ts';
 import { validate } from '../middlewares/validate.ts';
 
 const router = express.Router();
 
 router.post('/request', authMiddleware, sendFriendRequest);
 router.post(
-  '/request/:requestId/accept',
+  '/requests/:requestId/accept',
   authMiddleware,
-  validate(acceptFriendRequestParamsDTO, 'params'),
+  validate(acceptRejectFriendRequestParamsDTO, 'params'),
   acceptFriendRequest,
 );
-router.post('/request/:requestId/reject', rejectFriendRequest);
+router.post(
+  '/requests/:requestId/reject',
+  authMiddleware,
+  validate(acceptRejectFriendRequestParamsDTO, 'params'),
+  rejectFriendRequest,
+);
 router.get('/requests', authMiddleware, getPendingFriendRequests);
 router.get('/', authMiddleware, getFriends);
 
